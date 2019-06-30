0ce8327033b67a7beb69c3d4e7346eeb75c2c1c2
Fatih <budakf@itu.edu.tr>
2019-07-01 01:43:11 +0300

    added writer book and cart tables

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
+from .models import Book, Cart, Writer
 
 # Register your models here.
+
+class WriterAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+    list_display_links = list_display
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['book_name', 'book_price', 'book_published_date']
+    list_display_links = list_display
+
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['total_fee']
+
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Writer, WriterAdmin)
+admin.site.register(Cart, CartAdmin)
+
 
--- main/models.py
+++ main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+
+class Writer(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name  = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=80)
+    book_writer = models.ManyToManyField(Writer,related_name='writers')
+    book_published_date = models.DateField(default=datetime.now())
+    book_price = models.DecimalField(max_digits=6, decimal_places=3)
+    book_detail = models.TextField()
+
+    def __str__(self):
+        return self.book_name
+
+
+class Cart(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, default=1)
+    book_number = models.IntegerField()
+    total_fee = models.DecimalField(max_digits=10, decimal_places=3)
+
+
+    def __str__(self):
+        return self.owner
+
+
